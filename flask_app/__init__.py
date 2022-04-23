baaf54c48124f8f62cb2c4e68b0e11acedfbce50
“luis52” <ricardoluis6212@gmail.com>
2022-04-22 23:59:08 -0700

    test2

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -4,11 +4,11 @@ import git
 app = Flask(__name__)
 app.secret_key = "randomstring83209480299-0234"
 
-@app.route('/git_update',methods=['POST'])
+@app.route('/git_update', methods=['POST'])
 def git_update():
-    repo = git.Repo('./Todo_App')
-    origin = repo.remotes.origin
-    repo.create_head('main',
-    origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
-    origin.pull()
-    return '', 200
+  repo = git.Repo('./Todo_App')
+  origin = repo.remotes.origin
+  repo.create_head('main', 
+  origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+  origin.pull()
+  return '', 200

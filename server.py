6ee7b750d67587971e32f19141436382349a3a71
“luis52” <ricardoluis6212@gmail.com>
2022-04-22 23:08:33 -0700

    fixed an import issue in server.py

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from flask_app import app
-from flask_app.controllers import tasks
+#had an issue here importing because of an empty space causing import issues
+#now resolved
+from flask_app.controllers import tasks 
 
 if __name__=="__main__":
     app.run(debug=True, host='127.0.0.1', port=5001)

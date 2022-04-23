4f6f4a6553252292f208abfaa4ae15090a0bb93d
“luis52” <ricardoluis6212@gmail.com>
2022-04-22 23:53:01 -0700

    edited init and server files

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,4 +1,14 @@
 from flask import Flask
+import git
 
 app = Flask(__name__)
 app.secret_key = "randomstring83209480299-0234"
+
+@app.route('/git_update',methods=['POST'])
+def git_update():
+    repo = git.Repo('./Todo_App')
+    origin = repo.remotes.origin
+    repo.create_head('main',
+    origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return '', 200
--- server.py
+++ server.py
@@ -3,5 +3,7 @@ from flask_app import app
 #now resolved
 from flask_app.controllers import tasks 
 
+
 if __name__=="__main__":
     app.run(debug=True, host='127.0.0.1', port=5001)
+
